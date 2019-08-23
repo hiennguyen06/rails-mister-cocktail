36935db828e942c6f70432b387eb41667aa62b1e
Hien Nguyen <hiennguyen1529@gmail.com>
2019-08-23 10:53:17 +1000

    index and new front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "soda")
+Ingredient.create(name: "cola")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Negroni")
+Cocktail.create(name: "Martini")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Old Fashion")
+Cocktail.create(name: "Mojito")
+
+puts "Finished"
+
+
+
+
+
+
+
+
